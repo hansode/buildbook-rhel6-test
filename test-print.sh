8ef95478e518c93168f0eb08b72f9541e816ff84
Masahito Yoshida <masahito@axsh.net>
2013-11-01 21:43:24 +0900

    cleanup box-disk1.raw.

--- test-print.sh
+++ test-print.sh
@@ -8,15 +8,18 @@ set -x
 
 [[ -d tmp ]] || mkdir tmp
 
+readonly vmbuilder_conf_path=./vmbuilder.conf
+
 for title in vendor/buildbook-rhel6/*; do
   [[ -d "${title}" ]] || continue
   (
    echo "===> ${title##*/} started"
    cd ${title}
-   pwd; ls
-   ln -fs ../../../vmbuilder.conf ./.
-   ../../vmbuilder/kvm/rhel/6/vmbuilder.sh --config-path ./vmbuilder.conf
-   rm -f ./vmbuilder.conf
+
+   ln -fs ../../../vmbuilder.conf ${vmbuilder_conf_path}
+   ../../vmbuilder/kvm/rhel/6/vmbuilder.sh --config-path ${vmbuilder_conf_path}
+   rm -f box-disk1.raw
+   rm -f ${vmbuilder_conf_path}
    echo "===> ${title##*/} finished"
   )
 done
