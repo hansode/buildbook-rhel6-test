f922823473deb53fd683f66e9c7f7edcd66552be
Masahito Yoshida <masahito@axsh.net>
2013-11-04 19:38:28 +0900

    add "time" command.

--- test-print.sh
+++ test-print.sh
@@ -15,7 +15,7 @@ for title in vendor/buildbook-rhel6/${1:-"*"}; do
    cd ${title}
 
    ln -fs ../../../vmbuilder.conf ${vmbuilder_conf_path}
-   ../../vmbuilder/kvm/rhel/6/vmbuilder.sh --config-path ${vmbuilder_conf_path}
+   time ../../vmbuilder/kvm/rhel/6/vmbuilder.sh --config-path ${vmbuilder_conf_path}
    rm -f box-disk1.raw
    rm -f ${vmbuilder_conf_path}
    echo "===> ${title##*/} finished"
